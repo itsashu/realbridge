c402e03c1c87028a651982b42c882b7643ebac16
Kumar Ashutosh <KAshutosh@dealersocket.com>
2022-05-18 17:51:35 +0530

    Added initial database setup script

---
+++ database/InitialDatabaseSetupScript.sql
@@ -0,0 +1,42 @@
+------------------------------Database setup initial script---------------------------
+IF DB_ID('RealBridgeDB') IS NULL
+	BEGIN
+		PRINT 'Creating RealBridgeDB Database';
+		CREATE Database RealBridgeDB;
+		PRINT 'RealBridgeDB created';
+	END
+ELSE 
+	BEGIN
+		PRINT 'RealBridgeDB already exist';
+	END
+
+GO
+
+USE RealBridgeDB
+PRINT 'Using RealBridgeDB';
+
+GO
+
+IF OBJECT_ID (N'Images', N'U') IS NULL 
+	BEGIN
+		PRINT 'creating new table: Images';
+		CREATE TABLE Images (
+			ImageId int identity primary key,
+			ImageTitle nvarchar(20),
+			ImageDescription nvarchar(20) null,
+			ImageData varbinary(max) null,
+		);
+		PRINT 'Created new table: Images';
+	END
+ELSE
+	BEGIN
+		PRINT 'Images table already exists';	
+	END
+
+GO
+
+PRINT 'Adding Dummy Data to Images Table';
+
+INSERT INTO Images VALUES ('1st dummy image', 'dummy image title', null);
+
+select * from Images
